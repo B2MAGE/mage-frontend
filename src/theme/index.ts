5c01a01a43c61322a94262313a52862fc50e3051
Peter Bucci <sb1285n@gmail.com>
2026-04-21 13:32:20 -0400

    refactor(theme): split theme runtime and context into explicit module files

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,11 +1,16 @@
-export { ThemeProvider, useTheme } from './ThemeProvider'
+export { ThemeProvider } from './ThemeProvider'
+export { useTheme } from './themeContext'
+export { APP_THEME_STORAGE_KEY } from './runtime'
 export {
   APP_THEME_DEFINITIONS,
   APP_THEME_OPTIONS,
-  APP_THEME_STORAGE_KEY,
   APP_THEMES,
   DEFAULT_APP_THEME_ID,
   getAppTheme,
   isAppThemeId,
 } from './themes'
-export type { AppThemeColorScheme, AppThemeDefinition, AppThemeId } from './themes'
+export type {
+  AppThemeColorScheme,
+  AppThemeDefinition,
+  AppThemeId,
+} from './themes'
